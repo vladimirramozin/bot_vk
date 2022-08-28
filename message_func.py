7b9b3c99ce20f56ee8e5e3b5e1c239eb145b19b7
Владимир <vladimirramozin@yandex.ru>
2022-08-28 13:36:34 +0300

    по тестить

--- message_func.py
+++ message_func.py
@@ -45,6 +45,12 @@ handler.setFormatter(formatter)
 
 
 def execute_read_query(connection_bd, query):
+    '''
+    Функция посылает запрос в БД 
+    postgres принимает два аргумента:
+    connection_bd, соединение к бд через psycopg2.connect,
+    query, SQL запрос SELECT..
+    '''
     cursor = connection_bd.cursor()
     try:
         cursor.execute(query)
@@ -55,6 +61,17 @@ def execute_read_query(connection_bd, query):
 
 
 def preparing_images_for_sending(category, vk, con, upload, user_id, key=None):
+    '''
+    Подготавливает представление изображения к виду
+    необходимому для отправку в VK, принимает аргументы:
+    category, категория по которой делается выборка в бд,
+    vk, подсоединение к чату в VK, 
+    con, соединение с БД,
+    upload, результат VkUpload(vk),
+    user_id, идентификатор пользователя в VK,
+    key, варианты клавиатур для ответов,
+    после завершения работы передает данные в write_msg()
+    '''
     select_shop = (
         'SELECT products, photo FROM shop WHERE category = {}'.format(category)
     )
@@ -71,12 +88,20 @@ def preparing_images_for_sending(category, vk, con, upload, user_id, key=None):
             )
             write_msg(user_id, f'{photo[0]}', vk, attachmets, key)
             logger.info('файл картинки успешно прочитан')
-        return attachmets
     except FileNotFoundError:
         logger.error('файл не найден')
 
 
 def write_msg(user_id, message, vk, attachment=None, keyboard=None):
+    '''
+    Передает сообщение в VK, принимает аргументы:
+    user_id, идентификатор пользователя в VK,
+    message, строка сообщения,
+    vk, подсоединение к чату в VK,
+    attachment, подготовленное в preparing_images_for_sending() 
+    для отправки в ВК изобржение,
+    key, варианты клавиатур для ответов
+    '''
     if keyboard == 'keyboard_for_shop':
         return vk.method(
             'messages.send', {
